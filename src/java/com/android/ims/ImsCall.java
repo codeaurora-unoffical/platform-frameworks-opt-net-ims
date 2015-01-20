LineageOS/android_frameworks_opt_net_ims

Update only active participants in Conference list
There are participant entries in the CEP xml information from
modem who are already in DISCONNECTED state. The participants
list preparation does not take care of discarding them due to which
too many telecomm call objects are created and removed.

Hence, the list preparation is modified to discard DISCONNECTED
participants

Change-Id: I83e20b3e6800f63e4c4b6f8726220ab76ec29c00
CRs-Fixed: 773866

src/java/com/android/ims/ImsCall.java
@@ -1691,9 +1691,11 @@ public class ImsCall implements ICall {
                 Uri endpointUri = Uri.parse(endpoint);
                 int connectionState = ImsConferenceState.getConnectionStateForStatus(status);
 
-                ConferenceParticipant conferenceParticipant = new ConferenceParticipant(handle,
-                        displayName, endpointUri, connectionState);
-                mConferenceParticipants.add(conferenceParticipant);
+                if (connectionState != Connection.STATE_DISCONNECTED) {
+                    ConferenceParticipant conferenceParticipant = new ConferenceParticipant(handle,
+                            displayName, endpointUri, connectionState);
+                    mConferenceParticipants.add(conferenceParticipant);
+                }
                 continue;
             }
 

codeaurora-unoffical/platform-frameworks-opt-net-ims

src/java/com/android/ims/ImsCall.java
@@ -1689,9 +1689,11 @@ public class ImsCall implements ICall {
                 Uri endpointUri = Uri.parse(endpoint);
                 int connectionState = ImsConferenceState.getConnectionStateForStatus(status);
 
-                ConferenceParticipant conferenceParticipant = new ConferenceParticipant(handle,
-                        displayName, endpointUri, connectionState);
-                conferenceParticipants.add(conferenceParticipant);
+                if (connectionState != Connection.STATE_DISCONNECTED) {
+                    ConferenceParticipant conferenceParticipant = new ConferenceParticipant(handle,
+                            displayName, endpointUri, connectionState);
+                    conferenceParticipants.add(conferenceParticipant);
+                }
                 continue;
             }
 