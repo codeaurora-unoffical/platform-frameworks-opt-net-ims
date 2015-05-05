faef83878dba89f616da16e9e0e0869d523a88d7
Omkar Kolangade <omkark@codeaurora.org>
2015-05-04 17:55:13 -0700

    IMS RAT Reporting in Registration Callbacks
    Changing callback signatures in ImsManager to
    facilitate propagation of IMS Radio Access
    Technology information at the time of IMS
    registration.
    
    Change-Id: I18f77f502ffae152cca1f53b31698358f9a869b9
    CRs-Fixed: 834265

--- src/java/com/android/ims/ImsManager.java
+++ src/java/com/android/ims/ImsManager.java
@@ -40,7 +40,6 @@ import com.android.ims.internal.IImsService;
 import com.android.ims.internal.IImsUt;
 import com.android.ims.internal.ImsCallSession;
 import com.android.ims.internal.IImsConfig;
-
 import com.android.internal.telephony.TelephonyProperties;
 
 import java.util.HashMap;
@@ -869,9 +868,11 @@ public class ImsManager {
         }
 
         @Override
-        public void registrationConnected() {
+        public void registrationConnected(int imsRadioTech) {
+            // Note: imsRadioTech value maps to RIL_RADIO_TECHNOLOGY
+            //       values in ServiceState.java.
             if (DBG) {
-                log("registrationConnected ::");
+                log("registrationConnected :: imsRadioTech=" + imsRadioTech);
             }
 
             if (mListener != null) {
@@ -880,9 +881,11 @@ public class ImsManager {
         }
 
         @Override
-        public void registrationProgressing() {
+        public void registrationProgressing(int imsRadioTech) {
+            // Note: imsRadioTech value maps to RIL_RADIO_TECHNOLOGY
+            //       values in ServiceState.java.
             if (DBG) {
-                log("registrationProgressing ::");
+                log("registrationProgressing :: imsRadioTech=" + imsRadioTech);
             }
 
             if (mListener != null) {
